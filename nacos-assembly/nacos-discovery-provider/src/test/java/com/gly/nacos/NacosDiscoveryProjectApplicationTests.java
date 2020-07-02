01e6bc1046ab238000c0bb800c510f0a0078f274
lvye.guo <1689392164@qq.com>
2020-07-02 11:32:52 +0800

    nacos服务发现提供者

---
+++ nacos-assembly/nacos-discovery-provider/src/test/java/com/gly/nacos/NacosDiscoveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gly.nacos;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosDiscoveryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
